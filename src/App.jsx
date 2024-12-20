f19e3d0e1d179b1915d4689a98e4cbe8dc442695
24LP8229 24LP8229 <24LP8229@admins-iMac.local>
2024-12-20 12:32:56 +0800

    gogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Cover } from "./components/Cover";
 import { Header } from "./components/Header";
-import { Story } from "./components/Story";
+import { Gogo, Story } from "./components/Story";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Cover />
+      <Gogo />
       <Story />
     </div>
   );
